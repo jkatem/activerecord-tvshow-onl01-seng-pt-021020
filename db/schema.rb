a398fd16c3c72ca6437be9f3d8f3cd87b3220a3b
jkatem <jenmiao@gmail.com>
2020-05-01 08:54:39 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_232510) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
